91a3e6a5d69a73b7e783518078d92af829a48e33
aburahan8721 <aburaihan8721@gmail.com>
2024-01-14 12:26:56 +0600

    initial commit

---
+++ src/components/menuLink/MenuLink.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prop-types */
+import { useSelector } from "react-redux";
+import "./menulink.css";
+
+export default function MenuLink({ icon, text }) {
+  const name = useSelector((state) => state.user.userInfo.name);
+  return (
+    <div className="menulink">
+      {icon}
+      <span className="menuLinkText">{text}</span>
+      <span className="menuLinkTextName">{text === "Logout" && `( ${name} )`}</span>
+    </div>
+  );
+}
